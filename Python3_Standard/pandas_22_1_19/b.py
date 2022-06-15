8f4a2909a95e8335f86fb116c3023141270bde5e
5323368@qq.com <5323368@qq.com>
2022-06-15 08:43:07 +0800

    q

--- Python3_Standard/pandas_22_1_19/b.py
+++ Python3_Standard/pandas_22_1_19/b.py
@@ -4,11 +4,6 @@ df1 = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
 print(df1)
 df2 = pd.DataFrame({'a': [2, 2, 4], })
 print(df2)
-df=pd.merge(df1,df2,how='left')
+# 行的笛卡尔积
+df = pd.merge(df1, df2, how='left',on='a')
 print(df)
-
-
-
-
-
-
