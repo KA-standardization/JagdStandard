68878f13e06d0dd2b12db56f6afbb411029b583a
王松林 <songlin.wang@aisuperstar.com>
2022-07-07 18:07:03 +0800

    w7_7

---
+++ CPlusPlus_Standard/test_c/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int bar(int x, int y, int z) {
+    
+    return 1;
+}
+
+int main() {
+    
+    bar(1, 2, 3);
+
+    return 0;
+}
---
+++ Python3_Standard/ctypes_22_2_25/test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int bar(int x,int y,int z){
+    return 1;
+}
+
+int main(){
+    bar(1,2,3);
+    return 0;
+}
