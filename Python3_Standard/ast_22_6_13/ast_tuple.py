ae577ba3de799d90f426a7055794423a9499f170
王松林 <songlin.wang@aisuperstar.com>
2022-07-27 15:36:12 +0800

    w727

--- Python3_Standard/ast_22_6_13/ast_tuple.py
+++ Python3_Standard/ast_22_6_13/ast_tuple.py
@@ -1,7 +1,43 @@
 import ast
+import json
 
-s = ("abc", 123, "true")
-# s2 = str(s)
-s2 = repr(s)
-s3 = ast.literal_eval(s2)
-print(type(s3), s3)
+
+# s = ("abc", 123, "true")
+# # s2 = str(s)
+# s2 = repr(s)
+# s3 = ast.literal_eval(s2)
+# print(type(s3), s3)
+
+def foo():
+    print("foo()")
+
+
+def bar():
+    print("bar")
+
+
+def default():
+    print("default")
+
+
+register = {
+    'foo': foo.__code__.co_name,
+    'bar': bar.__code__.co_name,
+}
+
+if __name__ == '__main__':
+    # print(register.__repr__())
+    #
+    # print(foo.__code__.co_name)
+    # repr_register = repr(register)
+    repr_register = json.dumps(register)
+
+
+    def m(key):
+        funcs = ast.literal_eval(repr_register)
+        # ast.literal_eval(funcs.get(key))
+        eval(funcs.get(key))()
+
+
+    m('foo')
+
--- Python3_Standard/base_struct_7_30/内存管理.py
+++ Python3_Standard/base_struct_7_30/内存管理.py
@@ -2,4 +2,12 @@
 https://www.evanjones.ca/python-memory.html
 https://www.evanjones.ca/python-memory-part2.html
 https://www.evanjones.ca/python-memory-part3.html
+
+https://www.evanjones.ca/memoryallocator/
+"""
+
+"""
+Python 的内存分配器，称为 pymalloc，由 Vladimir Marangozov 编写，最初是 Python 2.1 和 2.2 的实验性功能，在 2.3 中默认启用之前。
+Python 使用了很多经常被创建和销毁的小对象，并且为每个对象调用malloc()and会带来很大的开销。free()为了避免这种情况，
+pymalloc 以 256 kB 的块分配内存，称为 arenas。arena 被划分为 4 kB 的池，这些池又被细分为固定大小的块
 """
